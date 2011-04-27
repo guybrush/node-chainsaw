93537de73eab17a7a182d851b8dd884cea5c3039
James Halliday <mail@substack.net>
2011-04-26 23:46:41 -0700

    passing tests for delayed nested actions

--- test/chainsaw.js
+++ test/chainsaw.js
@@ -92,6 +92,70 @@ exports.nest = function () {
     ;
 };
 
+exports.nestWait = function () {
+    var ch = (function () {
+        var vars = {};
+        return Chainsaw(function (saw) {
+            this.do = function (cb) {
+                saw.nest(cb, vars);
+            };
+            
+            this.wait = function (n) {
+                setTimeout(function () {
+                    saw.next();
+                }, n);
+            };
+        });
+    })();
+    
+    var order = [];
+    var to = setTimeout(function () {
+        assert.fail("Didn't get to the end");
+    }, 1000);
+    
+    var times = {};
+    
+    ch
+        .do(function (vars) {
+            vars.x = 'y';
+            order.push(1);
+            
+            this
+                .do(function (vs) {
+                    order.push(2);
+                    vs.x = 'x';
+                    times.x = Date.now();
+                })
+                .wait(50)
+                .do(function (vs) {
+                    order.push(3);
+                    vs.z = 'z';
+                    
+                    times.z = Date.now();
+                    var dt = times.z - times.x;
+                    assert.ok(dt >= 50 && dt < 75);
+                })
+            ;
+        })
+        .do(function (vars) {
+            vars.y = 'y';
+            order.push(4);
+            
+            times.y = Date.now();
+        })
+        .wait(100)
+        .do(function (vars) {
+            assert.eql(order, [1,2,3,4]);
+            assert.eql(vars, { x : 'x', y : 'y', z : 'z' });
+            clearTimeout(to);
+            
+            times.end = Date.now();
+            var dt = times.end - times.y;
+            assert.ok(dt >= 100 && dt < 125)
+        })
+    ;
+};
+
 exports.builder = function () {
     var cx = Chainsaw(function (saw) {
         this.x = function () {};
