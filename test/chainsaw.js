5086c94d0c048d6372c896c8897ea860efd78ad1
James Halliday <mail@substack.net>
2011-04-27 23:46:25 -0700

    passing test for auto next in nest

--- test/chainsaw.js
+++ test/chainsaw.js
@@ -156,6 +156,62 @@ exports.nestWait = function () {
     ;
 };
 
+exports.nestNext = function () {
+    var ch = (function () {
+        var vars = {};
+        return Chainsaw(function (saw) {
+            this.do = function (cb) {
+                saw.nest(false, function () {
+                    var args = [].slice.call(arguments);
+                    args.push(saw.next);
+                    cb.apply(this, args);
+                }, vars);
+            };
+        });
+    })();
+    
+    var order = [];
+    var to = setTimeout(function () {
+        assert.fail("Didn't get to the end");
+    }, 500);
+    
+    var times = [];
+    
+    ch
+        .do(function (vars, next_) {
+            vars.x = 'y';
+            order.push(1);
+            
+            this
+                .do(function (vs, next) {
+                    order.push(2);
+                    vs.x = 'x';
+                    setTimeout(next, 30);
+                })
+                .do(function (vs, next) {
+                    order.push(3);
+                    vs.z = 'z';
+                    setTimeout(next, 10);
+                })
+                .do(function () {
+                    setTimeout(next_, 20);
+                })
+            ;
+        })
+        .do(function (vars, next) {
+            vars.y = 'y';
+            order.push(4);
+            setTimeout(next, 5);
+        })
+        .do(function (vars) {
+            assert.eql(order, [1,2,3,4]);
+            assert.eql(vars, { x : 'x', y : 'y', z : 'z' });
+            
+            clearTimeout(to);
+        })
+    ;
+};
+
 exports.builder = function () {
     var cx = Chainsaw(function (saw) {
         this.x = function () {};
